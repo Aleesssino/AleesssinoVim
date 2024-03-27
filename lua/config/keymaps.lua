8c2e5c0e29f0cb78f515b2664e2b4f9f40798c5a
Aleš Gábor <alesg3a1b4o1@gmail.com>
2024-03-27 17:36:08 +0100

    keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,11 @@
 -- Add any additional keymaps here
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+local Util = require("lazy.util")
+
+keymap.set("n", "<C-_>", function()
+  Util.float_term(nil, { border = "rounded" })
+end, { desc = "Term with border" })
 
 keymap.set("n", "x", '"_x')
 
@@ -17,16 +22,6 @@ keymap.set("n", "<C-a>", "gg<S-v>G")
 keymap.set("n", "<Leader>w", ":update<Return>", opts)
 keymap.set("n", "<Leader>q", ":quit<Return>", opts)
 keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
-
--- File explorer with NvimTree
-keymap.set("n", "<Leader>f", ":NvimTreeFindFile<Return>", opts)
-keymap.set("n", "<Leader>t", ":NvimTreeToggle<Return>", opts)
-
--- Tabs
-keymap.set("n", "te", ":tabedit")
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
-keymap.set("n", "tw", ":tabclose<Return>", opts)
 
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
