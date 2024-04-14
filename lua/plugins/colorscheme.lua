13270f5c19be5f4efa2fb51bd7c2b366fc485ab4
Aleesssino <alesg3a1b4o1@gmail.com>
2024-04-14 22:01:31 +0200

    fixing autocmds

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,12 +7,9 @@ return {
     opts = function()
       return {
         transparent = true,
-<<<<<<< HEAD
         styles = {
           sidebars = "transparent",
         },
-=======
->>>>>>> cdc148ae5917d5e72824aeb85a299e14f03f2ab4
       }
     end,
   },
