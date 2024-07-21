02c030fc9ecaf8f2cd6a171c353478ade6f87117
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-21 05:27:24 +0200

    feat: Make float windows transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,8 +7,10 @@ return {
     opts = function()
       return {
         transparent = true,
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim]
         styles = {
           sidebars = "transparent",
+          floats = "transparent",
         },
       }
     end,
