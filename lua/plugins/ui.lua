a70d4c317b254b5af9882b26d207b14ad801fd65
Aleesssino <alesg3a1b4o1@gmail.com>
2024-07-23 02:28:00 +0200

    feat: Customize lualine (statusline)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -61,14 +61,95 @@ return {
     end,
   },
 
-  -- statusline
+  -- lualine.lua
+  -- Custom status line
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = {
-      options = {
-        theme = "solarized_dark",
-      },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
+    event = "VeryLazy",
+    config = function()
+      -- Custom Lualine component to show attached language server
+      local clients_lsp = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        local clients = vim.lsp.buf_get_clients(bufnr) -- Don't change, breaks the function
+        if next(clients) == nil then
+          return ""
+        end
+
+        local c = {}
+        for _, client in pairs(clients) do
+          table.insert(c, client.name)
+        end
+        return " " .. table.concat(c, "|")
+      end
+
+      local custom_osaka = require("lualine.themes.auto")
+
+      -- Custom colours
+      custom_osaka.normal.b.fg = "#cad3f5"
+      custom_osaka.insert.b.fg = "#cad3f5"
+      custom_osaka.visual.b.fg = "#cad3f5"
+      custom_osaka.replace.b.fg = "#cad3f5"
+      custom_osaka.command.b.fg = "#cad3f5"
+      custom_osaka.inactive.b.fg = "#cad3f5"
+      custom_osaka.normal.c.fg = "#6e738d"
+      custom_osaka.normal.c.bg = "NONE"
+
+      require("lualine").setup({
+        options = {
+          theme = custom_osaka,
+          component_separators = "",
+          section_separators = { left = "", right = "" },
+          disabled_filetypes = { "alpha" },
+        },
+        sections = {
+          lualine_a = {
+            { "mode", separator = { left = " ", right = "" }, icon = "" },
+          },
+          lualine_b = {
+            {
+              "filetype",
+              icon_only = true,
+              padding = { left = 1, right = 0 },
+            },
+            "filename",
+          },
+          lualine_c = {
+            {
+              "branch",
+              icon = "",
+            },
+            {
+              "diff",
+              symbols = { added = " ", modified = " ", removed = " " },
+              colored = false,
+            },
+          },
+          lualine_x = {
+            {
+              "diagnostics",
+              symbols = { error = " ", warn = " ", info = " ", hint = "" },
+              update_in_insert = true,
+            },
+          },
+          lualine_y = { clients_lsp },
+          lualine_z = {
+            { "location", separator = { left = "", right = " " }, icon = "" },
+          },
+        },
+        inactive_sections = {
+          lualine_a = { "filename" },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { "location" },
+        },
+        extensions = { "toggleterm", "trouble" },
+      })
+    end,
   },
 }
