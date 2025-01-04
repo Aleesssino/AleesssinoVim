ba2ffae23f5a843b9ceca45087900d33ae590520
Aleesssino <alesg3a1b4o1@gmail.com>
2025-01-04 23:48:59 +0100

    chore: Update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,7 +13,7 @@ keymap.set("n", "x", '"_x')
 
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+keymap.set("n", "=", "<C-x>")
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
@@ -24,29 +24,42 @@ keymap.set("n", "<Leader>q", ":quit<Return>", opts)
 keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
 
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- keymap.set("n", "ss", ":split<Return>", opts)
+-- keymap.set("n", "sv", ":vsplit<Return>", opts)
 
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+-- keymap.set("n", "sk", "<C-w>k")
+-- keymap.set("n", "sj", "<C-w>j")
+-- keymap.set("n", "sh", "<C-w>h")
+-- keymap.set("n", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set("n", "<C-S-h>", "<C-w><")
-keymap.set("n", "<C-S-l>", "<C-w>>")
-keymap.set("n", "<C-S-k>", "<C-w>+")
-keymap.set("n", "<C-S-j>", "<C-w>-")
+-- keymap.set("n", "<C-S-h>", "<C-w><")
+-- keymap.set("n", "<C-S-l>", "<C-w>>")
+-- keymap.set("n", "<C-S-k>", "<C-w>+")
+-- keymap.set("n", "<C-S-j>", "<C-w>-")
 
 -- Oil vim
 keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 
+-- Paste over a word without overwriting the current register content
+keymap.set("n", "<Leader>P", '"_diw0P', { noremap = true, silent = true })
+
 -- Special
 -- open current file (html..) in browser
-keymap.set("n", "<Leader>io", ":!xdg-open %<CR>", opts)
+keymap.set("n", "<Leader>io", ":!xdg-open %<CR>", { desc = "open Image/File" })
 -- move Image
-keymap.set("n", "<leader>ip", ":MoveImage<CR>", { noremap = true, silent = true })
+keymap.set("n", "<Leader>ip", ":MoveImage<CR>", { noremap = true, silent = true })
+
+-- FastAction code action for normal mode
+keymap.set("n", "<Leader>a", "<CMD>lua require('fastaction').code_action()<CR>", { buffer = bufnr }, {
+  desc = "FastAction Code Action",
+})
+
+-- FastAction range code action for visual mode
+keymap.set("v", "<Leader>a", "<ESC><CMD>lua require('fastaction').range_code_action()<CR>", { buffer = bufnr }, {
+  desc = "FastAction Range Code Action",
+})
 
 -- Diagnostics
 keymap.set("n", "<C-j>", function()
